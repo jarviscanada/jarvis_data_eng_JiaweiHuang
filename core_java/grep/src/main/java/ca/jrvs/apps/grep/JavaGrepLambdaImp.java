2e0d7281c7a25159c892609e5e85c6f72994411d
JofHearts <jwhuang459@gmail.com>
2026-03-13 13:38:29 -0400

    revise stream logic

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepLambdaImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepLambdaImp.java
@@ -1,16 +1,13 @@
 package ca.jrvs.apps.grep;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.Collections;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class JavaGrepLambdaImp extends JavaGrepImp {
     public static void main(String[] args) {
@@ -28,55 +25,29 @@ public class JavaGrepLambdaImp extends JavaGrepImp {
         }
     }
 
-    @Override
-    public void process() throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(getOutFile()))) {
-            listFiles(getRootPath()).stream()
-                    .flatMap(file -> readLinesAsStream(file))
-                    .filter(this::containPattern)
-                    .forEach(line -> {
-                        try {
-                            writer.write(line);
-                            writer.newLine();
-                        } catch (IOException e) {
-                            throw new RuntimeException("Error writing to file", e);
-                        }
-                    });
-        }
-    }
 
-    private Stream<String> readLinesAsStream(File inputFile) {
+    @Override
+    public List<File> listFiles(String rootDir) {
         try {
-            // ISO_8859_1 to prevent MalformedInputException
-            return Files.lines(inputFile.toPath(), StandardCharsets.ISO_8859_1);
+            return Files.walk(Paths.get(rootDir))
+                    .filter(Files::isRegularFile)
+                    .map(Path::toFile)
+                    .collect(Collectors.toList());
         } catch (IOException e) {
-            throw new RuntimeException("Error reading file: " + inputFile.getName(), e);
+            throw new RuntimeException(e);
         }
-    }
-
-//    @Override
-//    public List<String> readLines(File inputFile) {
-//        try {
-//            return Files.lines(inputFile.toPath()).collect(Collectors.toList());
-//        }catch (IOException e){
-//            e.printStackTrace();
-//            return Collections.emptyList();
-//        }
-//    }
-
-    @Override
-    public List<File> listFiles(String rootDir){
-        File dir = new File(rootDir);
-        File[] files = dir.listFiles();
-        return Arrays.stream(files).flatMap(file -> file.isDirectory()
-                ? listFiles(file.getPath()).stream()
-                : Stream.of(file))
-                .collect(Collectors.toList());
     }
 
     @Override
     public List<String> readLines(File inputFile) {
-        return readLinesAsStream(inputFile).collect(Collectors.toList());
+        try {
+            // ISO_8859_1 to prevent MalformedInputException
+            return Files.lines(inputFile.toPath(), StandardCharsets.ISO_8859_1)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
 
